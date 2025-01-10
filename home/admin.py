1284d119812001b105c79f99b1b3a00e112cba8b
Most. Razia Sultana <raziasultana772233@gmail.com>
2025-01-10 23:38:09 +0600

    updated

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import CarouselItem,Fact,AboutUsContent,AboutUs,Service,Feature,\
+    FeatureIcon,BusinessInfo,Logo,Project
 # Register your models here.
+admin.site.register(CarouselItem)
+admin.site.register(Fact)
+admin.site.register(AboutUs)
+admin.site.register(AboutUsContent)
+admin.site.register(Service)
+admin.site.register(Feature)
+admin.site.register(FeatureIcon)
+admin.site.register(BusinessInfo)
+admin.site.register(Logo)
+admin.site.register(Project)
